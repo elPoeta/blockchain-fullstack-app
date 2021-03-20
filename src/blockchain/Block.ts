1749e5a1b463b75b3d2e4f28714ebf05a1259e37
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2021-03-19 21:53:58 -0300

    refactor block fix tests

--- src/blockchain/Block.ts
+++ src/blockchain/Block.ts
@@ -1,6 +1,39 @@
 
 export class Block {
-    constructor(public timestamp: Date, public lastHash: String, public hash: String, public data: String) { }
+    constructor(private _timestamp: Date, private _lastHash: String, private _hash: String, private _data: String) { }
+
+    get timestamp(): Date {
+        return this._timestamp;
+    }
+
+    set timestamp(timestamp: Date) {
+        this._timestamp = timestamp;
+    }
+
+
+    get lastHash(): String {
+        return this._lastHash;
+    }
+
+    set lastHash(lastHash: String) {
+        this._lastHash = lastHash;
+    }
+
+    get hash(): String {
+        return this._hash;
+    }
+
+    set hash(hash: String) {
+        this._hash = hash;
+    }
+
+    get data(): String {
+        return this._data;
+    }
+
+    set data(data: String) {
+        this._data = data;
+    }
 }
 
 
