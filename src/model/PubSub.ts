f4c4f4e85857c01ab7a0af38af2fa770da43a545
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-06-04 19:42:22 -0300

    subcribe and publish blockchain into pubsub class

--- src/model/PubSub.ts
+++ src/model/PubSub.ts
@@ -1,35 +1,50 @@
 import { createClient } from "redis";
+import { Block } from "./Block";
+import { Blockchain } from "./Blockchain";
 
 const CHANNELS = {
   TEST: "TEST",
+  BLOCKCHAIN: "BLOCKCHAIN"
 };
 
-class PubSub {
+export class PubSub {
   public publisher: ReturnType<typeof createClient> | null;
   public subscriber: ReturnType<typeof createClient> | null;
-  constructor() {
+  public blockchain: Blockchain;
+
+  constructor({ blockchain }: { blockchain: Blockchain }) {
+    this.blockchain = blockchain;
     this.publisher = createClient();
     this.subscriber = this.publisher.duplicate();
-    this.initPubSub(CHANNELS.TEST);
+    this.subscribeToChannels();
   }
 
-  async initPubSub(channel: string) {
+  async subscribeToChannels() {
     await this.subscriber?.connect();
+    Object.values(CHANNELS)
+      .forEach(channel => this.subscribeChannel(channel));
+  }
+
+  async subscribeChannel(channel: string) {
     await this.subscriber?.subscribe(channel, (message) => this.handleMessage(channel, message));
   }
 
   handleMessage(channel: string, message: string) {
-    console.log(`Message recieve from channel ${channel}. Message: ${message}`);
-
+    if (channel === CHANNELS.BLOCKCHAIN) {
+      const chain = JSON.parse(message) as Block[];
+      this.blockchain.replaceChain(chain);
+    } else {
+      console.log(`Message recieve from channel ${channel}. Message: ${message}`);
+    }
   }
 
-  async sendMessage({ channel, message }: { channel: string, message: string }) {
+  async publish({ channel, message }: { channel: string, message: string }) {
     await this.publisher?.connect();
     await this.publisher!.publish(channel, message);
   }
+
+  broadcastChain() {
+    this.publish({ channel: CHANNELS.BLOCKCHAIN, message: JSON.stringify(this.blockchain.chain) });
+  }
 }
-
-
-const testPubSub = new PubSub();
-testPubSub.sendMessage({ channel: CHANNELS.TEST, message: "el-poeta" });
 
