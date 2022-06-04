885b9c02a987474e11f8938b67bea7bc34f6bd54
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-06-04 19:18:35 -0300

    add redis pub/sub

--- src/model/PubSub.ts
+++ src/model/PubSub.ts
@@ -1,28 +1,35 @@
-import redis from "redis";
+import { createClient } from "redis";
 
 const CHANNELS = {
   TEST: "TEST",
 };
-class PubSub {
-  public publisher;
-  public subscriber;
-  constructor() {
-    this.publisher = redis.createClient();
-    this.subscriber = redis.createClient();
 
-    this.subscriber.subscribe(CHANNELS.TEST, (message) => {
-      console.log(message);
-    });
-    this.subscriber.on("message", (channel: string, message: string) =>
-      this.handleMessage(channel, message)
-    );
+class PubSub {
+  public publisher: ReturnType<typeof createClient> | null;
+  public subscriber: ReturnType<typeof createClient> | null;
+  constructor() {
+    this.publisher = createClient();
+    this.subscriber = this.publisher.duplicate();
+    this.initPubSub(CHANNELS.TEST);
+  }
+
+  async initPubSub(channel: string) {
+    await this.subscriber?.connect();
+    await this.subscriber?.subscribe(channel, (message) => this.handleMessage(channel, message));
   }
 
   handleMessage(channel: string, message: string) {
     console.log(`Message recieve from channel ${channel}. Message: ${message}`);
+
+  }
+
+  async sendMessage({ channel, message }: { channel: string, message: string }) {
+    await this.publisher?.connect();
+    await this.publisher!.publish(channel, message);
   }
 }
 
-const testPubSub = new PubSub();
 
-testPubSub.publisher.publish(CHANNELS.TEST, "foo");
+const testPubSub = new PubSub();
+testPubSub.sendMessage({ channel: CHANNELS.TEST, message: "el-poeta" });
+
