9ac75c8e23c8d7fdcf2566c0a77c4e9054d6ff88
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-06-04 15:19:16 -0300

    mine blockchain from api

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,17 @@ const app = express();
 
 const blockchain = new Blockchain();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/api/v1/blocks", (req: Request, res: Response) => {
   res.status(200).json({ blocks: blockchain.chain });
+});
+
+app.post("/api/v1/mine", (req: Request, res: Response) => {
+  const { data } = req.body;
+  blockchain.addBlock(data);
+  res.redirect("/api/v1/blocks");
 });
 
 const PORT = process.env.PORT || 4000;
