c59d38d77c8374161cc488ae914d8e85df8a1a8a
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-06-12 15:17:39 -0300

    style app

--- client/src/components/Block.jsx
+++ client/src/components/Block.jsx
@@ -1,10 +1,13 @@
 export const Block = ({ block }) => {
   const { timestamp, hash, data} = block; 
+  const shortenHash = `${hash.substring(0,15)}...`;
+  const dataString = JSON.stringify(data);
+  const shortenData = dataString.length > 50 ? `${dataString.substring(0,50)}...` : dataString;
   return(
-    <>
-    <h4>Date: {new Date(timestamp).toDateString()}</h4>
-    <h5>Hash: {hash}</h5>
-    <h6>{JSON.stringify(data)}</h6>
-    </>
+    <div className="border-2 border-blue-400 break-words p-4 text-xl flex flex-col gap-3">
+      <h4 className="text-pink-500">Date: {new Date(timestamp).toLocaleString()}</h4>
+      <h5 className="text-orange-400" data-hash={hash}>Hash: {shortenHash}</h5>
+      <h6 data-fulldata={data}>{shortenData}</h6>
+    </div>
   )
 }
--- client/src/components/Blocks.jsx
+++ client/src/components/Blocks.jsx
@@ -15,8 +15,8 @@ export const Blocks = () => {
   const renderBlocks = () =>  blocks.map(block => <Block key={block.hash} block={block}/>);
   
   return (
-    <div className="">
-      <h2>Blocks</h2>
+    <div className="grid gap-3 grid-cols-[500px]">
+      <h2 className="font-bold text-2xl text-center p-6">Blocks</h2>
       {renderBlocks()}
     </div>
   );
