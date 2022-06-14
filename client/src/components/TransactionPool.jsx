c48a7147b561e6138b22d32e6f4189e9a213ef4d
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-06-14 09:12:18 -0300

    mine tx

--- client/src/components/TransactionPool.jsx
+++ client/src/components/TransactionPool.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
-import { useState } from "react"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {toast} from 'react-toastify';
 import { Nav } from "./Nav";
 import { Transaction } from "./Transaction";
 
@@ -7,6 +9,7 @@ const POOL_INTERVAL_MS = 10000;
 
 export const TransactionPool = () => {
  const [transactionPoolMap, setTransactionPoolMap] = useState({});
+ const navigate = useNavigate();
 
  useEffect(() => {
   return async () => {
@@ -30,10 +33,22 @@ export const TransactionPool = () => {
   }
  },[transactionPoolMap]);
 
+ const fetchTransactions = async () => {
+   const response = await fetch('/api/v1/mine-transactions');
+   if(response.status === 200) {
+    toast.info('Success');
+    navigate('/blocks');
+   } else {
+    toast.error('the mine transactions block request did not complete');
+   }
+
+ } 
+
  return(
     <div className="grid justify-center align-middle p-4">
       <h3 className="text-2xl p-4">Transaction Pool</h3>
        <Nav to='/' title='Home'/>
+       <button className="bg-green-600 text-white p-1 m-3" onClick={fetchTransactions}>Mine Transactions</button>
        { Object.values(transactionPoolMap).map(transaction => (
         <div key={transaction.id}>
           <hr/>
